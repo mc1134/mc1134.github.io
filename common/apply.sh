da5548c4c1553c090c3d327efed3fc7a9250d9af
mc1134 <j.michael.chen@gmail.com>
2018-12-20 17:10:26 -0700

    Working on script 4

--- common/apply.sh
+++ common/apply.sh
@@ -125,12 +125,10 @@ do
 			echo ">>SUCCESS FOR FILE <$shortFile> APPLYING <$shortApplyer>"
 			# overwrite lines $lineOfFirst-$lineOfLast for $file with $applyer
 			#TODO
-			#sed -i """$lineOfFirst,$lineOfLastd""" "$applyee"
-			#sed -i """r "$applyer"""
-			#sed """$lineOfFirst,$lineOfLast d""" "$file"
-			#sed -i """(( $lineOfFirst-1 ))r $applyer""" "$file"
-			sed -i "11,23d" test2.txt
-			sed -i "10r header.html" test2.txt
+			sed -i """$lineOfFirst,$lineOfLast d""" "$file"
+			sed -i """(( $lineOfFirst-1 ))r $applyer""" "$file"
+			# sed -i "11,23d" test2.txt
+			# sed -i "10r header.html" test2.txt
 		else echo ">>FAILURE FOR FILE <$shortFile> APPLYING <$shortApplyer>; ERROR FLAG(S) $er"
 		fi
 		echo -e ">>>>firstnum: $lineOfFirst; lastnum: $lineOfLast\n"
